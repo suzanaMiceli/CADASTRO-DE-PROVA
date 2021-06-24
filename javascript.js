0f94c1eeb3815cc37398a153cdde3b33d21df8cd
suzanaMiceli <77465033+suzanaMiceli@users.noreply.github.com>
2021-06-24 01:24:06 -0300

    Dados mais organizados
    Apesar de o arquivo não estar funcional, o dados são enviados de forma organizada

--- javascript.js
+++ javascript.js
@@ -4,36 +4,70 @@ let numeroQuestao = document.querySelector("#numeroQ");
 let alternativaQuestao = document.querySelector(".alternativa-questao");
 let alternativaCerta = document.querySelector(".sim");
 let botaoEnviar = document.querySelector(".enviar-prova");
+let idProva = document.querySelector("#provaId");
+let botaoAdAlt = document.querySelector(".adcAlt");
+let divAternativa = document.querySelector("#adcAlternativa");
+let salvaQuestao = document.querySelector(".adc-questao");
+let idAlternativa = document.querySelector("#numeroA");
 
+// criei listas pra empurrar os valores
+let exams = [];
+let alternatives = [];
+let questions = [];
 
+//salva a alternativa
+botaoAdAlt.addEventListener("click", function (event) {
+  event.preventDefault();
 
+  alternatives.push({
 
+    Id: idAlternativa.value,
+    orderNumber: numeroQuestao.value,
+    description: alternativaQuestao.value,
+    isCorrect: alternativaCerta.value,
+
+  });
+  console.log(alternatives);
   
+
+});
+//salva a questao dentro junto com as alternativas
+salvaQuestao.addEventListener("click", function (event) {
+  event.preventDefault();
+
+
+  questions.push({
+    examId:idProva.value,
+    description: questao.value,
+    orderNumber: numeroQuestao.value,
+    alternatives,
+  });
+  
+  alternatives = [];
+  console.log(questions);
+});
+
+
+//envia as infos nas listas
 botaoEnviar.addEventListener("click", function (event) {
   event.preventDefault();
 
-//peguei informações dos inputs
-  let infoProva = tprova.value;
-  let infoQuestao = questao.value;
-  let infoNumeroQuestao = numeroQuestao.value;
-  let infoAlternativaQuestao = alternativaQuestao.value;
-  let infoAlternativaCerta = alternativaCerta.value
-	
   
-  let prova = [];
+  exams.push({
+    title: tprova.value,
+    Id: idProva.value,
 
-  prova.push({
-    title: infoProva,
-    questions: infoQuestao,
-    description: infoAlternativaQuestao,
-    examId: infoNumeroQuestao,
-    isCorrect: infoAlternativaCerta,
+  });
+  
+  exams.push({
+    questions,
   });
 
-  let data = { prova };
+
+  let data = {exams};
 
   //envia as infos  
-  const options = {
+  const opcao = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -41,19 +75,15 @@ botaoEnviar.addEventListener("click", function (event) {
     body: JSON.stringify(data)
   };
 
-  fetch('http://localhost:3000/exams', options);
+  fetch('http://localhost:3000/exams', opcao);
 
-  console.log(prova);
- 
- 
+
   alert("Prova enviada com sucesso!");
-  
+
   location.reload();
- 
-  
+
+
 });
-
-
 
 
 
