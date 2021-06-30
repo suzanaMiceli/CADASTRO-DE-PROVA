a2cc2a2ab98f4b4a1cbb57ca3ef51f44df0bf5db
suzanaMiceli <77465033+suzanaMiceli@users.noreply.github.com>
2021-06-30 01:00:03 -0300

    Update javascript.js
    melhorando a informação

--- javascript.js
+++ javascript.js
@@ -1,49 +1,46 @@
 let tprova = document.querySelector(".tProva");
 let questao = document.querySelector("#descricao-questao");
 let numeroQuestao = document.querySelector("#numeroQ");
-let alternativaQuestao = document.querySelector(".alternativa-questao");
-let alternativaCerta = document.querySelector(".sim");
 let botaoEnviar = document.querySelector(".enviar-prova");
 let idProva = document.querySelector("#provaId");
-let botaoAdAlt = document.querySelector(".adcAlt");
-let divAternativa = document.querySelector("#adcAlternativa");
-let salvaQuestao = document.querySelector(".adc-questao");
-let idAlternativa = document.querySelector("#numeroA");
+let salvaQuestao = document.querySelector(".salva-questao");
+let container = document.querySelector(".container");
+let div = document.querySelector(".alternativa-container");
+let adcAlter = document.querySelector(".adcAlt");
+let quantidade = document.querySelector("#quant");
 
-// criei listas pra empurrar os valores
+
 let exams = [];
 let alternatives = [];
 let questions = [];
 
-//salva a alternativa
-botaoAdAlt.addEventListener("click", function (event) {
+
+
+adcAlter.addEventListener("click", function (event) {
+  
   event.preventDefault();
-
-  alternatives.push({
-
-    Id: idAlternativa.value,
-    orderNumber: numeroQuestao.value,
-    description: alternativaQuestao.value,
-    isCorrect: alternativaCerta.value,
-
-  });
-  console.log(alternatives);
+  aumentaDisplay();
+  adicionaAlternativas();
+  
+  salvaQuestao.style.display = "block";
   
 
 });
-//salva a questao dentro junto com as alternativas
+
+
+//exclui alternativas  
+div.addEventListener("dblclick", function (event) {
+  for (let index = 0; index < 4; index++) {
+    div.removeChild(div.lastChild);
+  }
+});
+
+
+
 salvaQuestao.addEventListener("click", function (event) {
   event.preventDefault();
-
-
-  questions.push({
-    examId:idProva.value,
-    description: questao.value,
-    orderNumber: numeroQuestao.value,
-    alternatives,
-  });
-  
-  alternatives = [];
+  objetoQuestao();
+  resetaInput();
   console.log(questions);
 });
 
@@ -86,5 +83,91 @@ botaoEnviar.addEventListener("click", function (event) {
 });
 
 
+function adicionaAlternativas(form) {
 
+  for (let index = 0; index < quantidade.value; index++) {
 
+    let alterNumero = document.createElement("input");
+    alterNumero.setAttribute("id", `alterNum${index+1}`);
+    alterNumero.setAttribute("placeholder", "Nº");
+
+    let novaAl = document.createElement("input");
+    novaAl.setAttribute("id", `alter${index+1}`);
+    novaAl.setAttribute("placeholder", `Alternativa${index+1}`);
+    
+    let respostaCerta = document.createElement("input");
+    respostaCerta.setAttribute("type", "checkbox");
+    respostaCerta.setAttribute("id", `sim${index + 1}`);
+    respostaCerta.setAttribute("value", "false");
+    
+    //verifica se o valor do checkbox é true or false
+    respostaCerta.addEventListener("click", function () {
+      document.querySelector(`#sim${index+1}`).value = true;
+    });
+
+    let spamTexto = document.createElement("spam");
+    spamTexto.innerHTML = "Opção certa";
+    
+    
+
+    alterNumero = div.appendChild(alterNumero).classList.add("inputPequeno");
+    novaAl = div.appendChild(novaAl).classList.add("descricao");
+    respostaCerta = div.appendChild(respostaCerta).classList.add("sim");
+    spamTexto = div.appendChild(spamTexto);
+    
+  };
+}
+
+function objetoQuestao(index) {
+  for (let index = 0; index < quantidade.value; index++) {
+    
+    alternatives.push({
+
+      Id: document.querySelector(`#alterNum${index+1}`).value,
+      questionId: numeroQuestao.value, 
+      description: document.querySelector(`#alter${index+1}`).value,
+      isCorrect: document.querySelector(`#sim${index+1}`).value,
+
+    });
+
+  };
+  
+  questions.push({
+    examId: idProva.value,
+    description: document.querySelector("#descricao-questao").value,
+    orderNumber: numeroQuestao.value,
+    alternatives,
+  });
+}
+
+function resetaInput(form) {
+  numeroQuestao.value ="";
+  quantidade.value = "";
+  questao.value= "";
+  removeAlternativas();
+  salvaQuestao.style.display = "none";
+  diminuiDisplay();
+  alternatives = [];
+}
+
+function removeAlternativas(event) {
+  while (div.firstChild) {
+    div.removeChild(div.firstChild);
+  }
+};
+
+function aumentaDisplay(form) {
+
+  container.style.width = "600px";
+  botaoEnviar.style.marginLeft = "110px"
+
+  
+}
+
+function diminuiDisplay(form) {
+
+  container.style.width = "31em";
+  botaoEnviar.style.marginLeft = "70px";
+
+  
+}
